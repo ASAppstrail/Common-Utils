4a202d451950e27e7866f1c15007d6ff5aa76dd7
Arjun Shrivastava <arjun.shrivastav@appstrail.com>
2026-09-24 18:56:23 +0530

    Replace old code with React Native project

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+  preset: '@react-native/jest-preset',
 };
